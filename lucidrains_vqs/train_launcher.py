b0abf92e9b187fd8e4c7e73b17ccae0f56948bb1
ismail hamdaoui <ismail.hamdaoui@telecom-paris.fr>
2025-02-06 16:33:53 +0100

    cleaning v3.0.0

--- lucidrains_vqs/train_launcher.py
+++ lucidrains_vqs/train_launcher.py
@@ -1,6 +1,6 @@
-############################################################################
-#######################  import necessary libraries  #######################
-############################################################################
+########################################################################################################
+#####################################  import necessary libraries  #####################################
+########################################################################################################
 
 
 import numpy as np
@@ -25,6 +25,7 @@ from tqdm import tqdm
 from utils.acdc_dataset import *
 from utils.funcs import *
 from utils.vqvae import *
+from utils.launcher_utils import *
 
 import argparse
 import json
@@ -33,103 +34,13 @@ from datetime import datetime
 
 
 
-############################################################################
-######################## Parameters ######################
-############################################################################
-
-
-def print_arguments(args, parser):
-    """
-    Prints all arguments used in the script and highlights whether default values were used.
-    
-    Args:
-        args: The parsed arguments (from `parser.parse_args()`).
-        parser: The `argparse.ArgumentParser` object.
-    """
-    print("Arguments used in this script:")
-    print("\n" )
-    
-    # Get the default values from the parser
-    defaults = {action.dest: action.default for action in parser._actions if action.dest != "help"}
-    
-    # Iterate through the arguments and print their values
-    for arg_name, arg_value in vars(args).items():
-        if arg_name in defaults:
-            default_value = defaults[arg_name]
-            if arg_value == default_value:
-                # Highlight if the default value was used
-                print(f"{arg_name}: {arg_value} (default)")
-            else:
-                # Highlight if the user provided a value
-                print(f"{arg_name}: {arg_value} (user-provided)")
-        else:
-            # Handle cases where the argument doesn't have a default (e.g., --kwargs)
-            print(f"{arg_name}: {arg_value}")
-
-    print("-" * 50)
-
-
-
-def save_training_metadata(args, best_epoch, score, percentage):
-    """
-    Save all the parameters, hyper-parameters, and codebook usage in a JSON file.
-
-    Args:
-        args: The parsed command-line arguments (from argparse).
-        model: The deep learning model.
-        codebook_usage: A dictionary or list containing codebook usage information (optional).
-    """
-
-
-    # Get the current timestamp for the filename
-    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M")
-    filename = (args.model_name).replace('.pth', '.json')
-
-        # Parse kwargs arguments into a dictionary
-    kwargs_dict = {}
-    if args.kwargs:
-        for kwarg in args.kwargs:
-            if '=' in kwarg:
-                key, value = kwarg.split('=', 1)
-                kwargs_dict[key] = value
-
-
-    # Prepare the metadata dictionary
-    metadata = {
-        "model_parameters": {
-            "embedding_dim": args.D,
-            "num_embeddings": args.K,
-            "downsampling_factor": args.downsampling_factor,
-            "residual": args.use_residual,
-            "num_quantizers": args.num_quantizers,
-            "shared_codebook": args.shared_codebook,
-            "beta": args.beta,
-            "decay": args.decay,
-            "data_modality": args.data_modality,
-        },
-        "kwargs_arguments": kwargs_dict,  # Store additional kwargs arguments
-        "training_parameters": {
-            "batch_size": args.BATCH_SIZE,
-            "epochs":args.epochs,
-            "loss_function": args.loss_func,
-        },
-        "evaluation": {
-            "best_epoch" : best_epoch, 
-            "score" : score,
-            "codebook_usage" : percentage,
-        },
-        "timestamp": timestamp,
-    }
-
-    # Save the metadata to a JSON file
-    with open(filename, 'w') as f:
-        json.dump(metadata, f, indent=4)
-
-    print(f"( Training metadata saved to {filename} )")
+########################################################################################################
+############################################### Parameters #############################################
+########################################################################################################
 
 
 def train(): 
-    ######################    Parse command-line ################################.
+    ######################    Parse command-line Args    ######################
 
     parser = argparse.ArgumentParser(description="Train a model with specific configurations.")
 
